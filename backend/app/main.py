eabaa2ae102aa16ebb015e730c495be91965dca5
Bereket <bereketsiyum@gmail.com>
2025-01-17 23:40:59 +0900

    secure fastapi docs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
+import secrets
 from contextlib import asynccontextmanager
 from pathlib import Path
 
 import sentry_sdk
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 from fastapi.responses import FileResponse
 from fastapi.routing import APIRoute
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.staticfiles import StaticFiles
 from openai import OpenAI
 from starlette.middleware.cors import CORSMiddleware
@@ -33,6 +37,8 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
     lifespan=lifespan,
+    docs_url=None,
+    redoc_url=None,
 )
 
 # Ensure upload directory exists
@@ -54,6 +60,8 @@ if settings.all_cors_origins:
         allow_headers=["*"],
     )
 
+security = HTTPBasic()
+
 @app.get("/", tags=["root"])
 async def root():
     return {"message": "Hello From ContentGen"}
@@ -61,5 +69,33 @@ async def root():
 @app.get("/favicon.ico", tags=["favicon"])
 async def favicon():
     return FileResponse(file_storage_settings.UPLOAD_DIR + "/logo.svg")
+
+@app.get("/docs", tags=["docs"])
+async def docs(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, settings.FIRST_SUPERUSER)
+    correct_password = secrets.compare_digest(credentials.password, settings.FIRST_SUPERUSER_PASSWORD)
+
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+
+@app.get("/openapi.json", tags=["openapi"])
+async def openapi(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, settings.FIRST_SUPERUSER)
+    correct_password = secrets.compare_digest(credentials.password, settings.FIRST_SUPERUSER_PASSWORD)
+
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    return get_openapi(title=settings.PROJECT_NAME, version="1.0.0", routes=app.routes)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
